97f9709a7c338e9e40a15423169d8cb2cccba86d
TIGER <suraj23082002@gmail.com>
2025-12-03 16:01:07 +0530

    chore: configure project for deployment

--- src/utils/axios/axiosProtected.ts
+++ src/utils/axios/axiosProtected.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const axiosProtected = axios.create({
-    baseURL: "http://localhost:3000/api/protected",
+    // baseURL: "http://localhost:3000/api/protected",
+    baseURL: "https://tigerxclasses.vercel.app/api/protected",
 });
 
 export default axiosProtected;
--- src/utils/axios/axiosPublic.ts
+++ src/utils/axios/axiosPublic.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const axiosPublic = axios.create({
-    baseURL: "http://localhost:3000/api",
+    // baseURL: "http://localhost:3000/api",
+    baseURL: "https://tigerxclasses.vercel.app/api",
 });
 
 export default axiosPublic;
